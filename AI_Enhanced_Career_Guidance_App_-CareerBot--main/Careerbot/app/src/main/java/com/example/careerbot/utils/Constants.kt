51b69fb64eb60dd041f741aae4f76b7ab2849362
Deepak Singla <120936852+deepaksingla022@users.noreply.github.com>
2025-11-23 17:46:32 +0530

    Change GEMINI_API_KEY to placeholder value
    Updated GEMINI_API_KEY placeholder for security.

--- AI_Enhanced_Career_Guidance_App_-CareerBot--main/Careerbot/app/src/main/java/com/example/careerbot/utils/Constants.kt
+++ AI_Enhanced_Career_Guidance_App_-CareerBot--main/Careerbot/app/src/main/java/com/example/careerbot/utils/Constants.kt
@@ -2,8 +2,8 @@ package com.example.careerbot.utils
 
 
 object Constants {
-  //  const val GEMINI_API_KEY = "AIzaSyDcEzc2qJEQ6JRRbjNnjUQzY6DqrArF7DQ" // deepak gemini key
-  const val GEMINI_API_KEY = "AIzaSyAOdsmgfWi6kyJfKrDl4KLcfRUsDvCNVLU"
+  const val GEMINI_API_KEY = "ADD GEMINI-KEY HERE" // deepak gemini keY
+ 
     const val FIRESTORE_USERS = "users"
     const val FIRESTORE_CHATS = "chats"
     const val MAX_HISTORY = 3
